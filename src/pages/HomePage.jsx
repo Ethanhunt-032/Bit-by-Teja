01ee3c2924444bce208df8d33e59d31fc084767b
Ethanhunt-032 <financewithteja@gmail.com>
2025-03-18 17:35:25 +0530

    Basic front end without node module

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
+import React from "react"
+
 export default function HomePage() {
     return (
-        <div>
-            <h1>Home Page !</h1>
-        </div>
-    )
+        <h1>Home page</h1>
+)
 }
