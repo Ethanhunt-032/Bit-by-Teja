a24c6d8bc657f9a76585415cbed6d1b330865b7c
Ethanhunt-032 <financewithteja@gmail.com>
2025-03-27 14:57:48 +0530

    changes in project structure

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,2 +1,8 @@
 import express from 'express';
 const app = express();
+
+app.get("/teja", (req, res) => {res.send("Hello Teja")})
+
+app.listen(8080,()=>{
+    console.log("Server started on port 8080")
+});
