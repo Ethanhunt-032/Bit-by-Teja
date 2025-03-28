e08c09051004d33aff49e771be411222e22740c9
Ethanhunt-032 <financewithteja@gmail.com>
2025-03-28 11:42:38 +0530

    dummy commit

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
 const app = express();
 
-app.get("/teja", (req, res) => {res.send("Hello Teja")})
+app.use(express.json());
+
+app.get("/teja", (req, res) =>
+    {res.send("Hello Teja")})
+
+app.get("/teja/:name", (req, res) =>{
+    res.send("Hello " + req.params.name);
+})
+
+app.post("/teja", (req, res) =>{
+    res.send("Hello Post request from" +req.body.name);
+})
+//  Req Body structure
+// {
+//     name : "teja dayana"
+// }
+
 
 app.listen(8080,()=>{
     console.log("Server started on port 8080")
