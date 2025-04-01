3e06818165a8185ce56e82188d53d50f8f3388c1
Ethanhunt-032 <financewithteja@gmail.com>
2025-04-01 09:35:41 +0530

    Random Tuesday commit

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,24 +1,25 @@
-import express from 'express';
-const app = express();
+import express, {response} from 'express';
 
+const articleInfo = [
+    {name:'learn-react',upvotes : 0},
+    {name:'learn-node',upvotes : 0},
+    {name:'learn-mongDb',upvotes : 0},
+];
+
+const app = express();
 app.use(express.json());
 
-app.get("/teja", (req, res) =>
-    {res.send("Hello Teja")})
-
-app.get("/teja/:name", (req, res) =>{
-    res.send("Hello " + req.params.name);
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const article = articleInfo.find(a=>a.name ===req.params.name);
+    article.upvotes += 1;
+    res.send("Success! now the article" + req.params.name + "has upvotes " + article.upvotes);
 })
-
-app.post("/teja", (req, res) =>{
-    res.send("Hello Post request from" +req.body.name);
+app.get('/hello',(req,res)=>{
+    console.log("Hello");
+    res.send("Hello")
 })
-//  Req Body structure
-// {
-//     name : "teja dayana"
-// }
 
 
 app.listen(8080,()=>{
-    console.log("Server started on port 8080")
+    console.log("Server started on port 8080");
 });
