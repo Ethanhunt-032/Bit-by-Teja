b5004740f1d5dece695b280012e2eabaed602772
Ethanhunt-032 <tejadayana11@gmail.com>
2025-04-21 23:30:59 +0530

    successfully connected mongo db with node server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,32 +1,61 @@
 import express, {response} from 'express';
-import {MongoClient, ServerApiVersion} from 'mongodb'
-
-const articleInfo = [
-    {name:'learn-react',upvotes : 0, comments:[]},
-    {name:'learn-node',upvotes : 0, comments:[]},
-    {name:'learn-mongDb',upvotes : 0, comments:[]},
-];
+import {MongoClient, ReturnDocument, ServerApiVersion} from 'mongodb'
 
 const app = express();
 app.use(express.json());
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const article = articleInfo.find(a=>a.name ===req.params.name);
-    article.upvotes += 1;
-    // res.send("Success! now the article" + req.params.name + "has upvotes " + article.upvotes);
+let db;
+
+async function connectToDB(){
+
+    const uri = 'mongodb://127.0.0.1:27017';
+    const client = new MongoClient(uri,{
+        serverApi:{
+            version: ServerApiVersion.v1,
+            strict:true,
+            deprecationErrors:true,
+        }
+    });
+
+    await client.connect();
+    db = client.db('full-stack-react-db');
+
+}
+
+app.get('/api/articles/:name',async (req,res)=>{
+    const {name} = req.params;
+    
+    const article = await db.collection('articles').findOne({ name });
     res.json(article);
 })
 
+app.post('/api/articles/:name/upvote',async (req, res) => {
+    const {name} = req.params;
+
+    const updateArticle = await db.collection('articles').findOneAndUpdate({ name },
+        {$inc:{upvotes: 1}},
+        {ReturnDocument: "after" }
+    );
+    res.json(updateArticle);
+})
+
+app.post("/api/articles/:name/comments",async (req,res)=>{
+    // const name = req.params.name
+    const {name} = req.params ;
+    const {postedBy, text} = req.body ;
+    const newComment = {postedBy, text};
+
+    const updateArticle =  await db.collection('articles').findOneAndUpdate({name},
+        {$push: {comments: newComment}},
+        {ReturnDocument: 'after'}
+    );
+
+    res.json(updateArticle);
+});
+
 app.post('/api/articles/:name', async (req, res) => {
     const {name} = req.params ;
-    const uri = 'mongodb://127.0.0.1:27017';
-    const client  = new MongoClient(
-        uri,
-        { serverApi: {
-            version : ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors:true
-    }})
+
     await client.connect();
     const db = client.db('fullstack-react-db');
     const artical = await db.collection('articles').findOne({name});
@@ -34,22 +63,19 @@ app.post('/api/articles/:name', async (req, res) => {
     res.json(artical);
 })
 
-app.post("/api/articles/:name/comments",(req,res)=>{
-    // const name = req.params.name
-    const {name} = req.params ;
-    const {postedBy, text} = req.params ;
-    const article = articleInfo.find(a=>a.name === name);
+async function start(){
+    await connectToDB();
+    app.listen(8080,()=>{
+        console.log("Server started on port 8080");
+    });
+}
 
-    article.comments.push({postedBy,text}) ;
-    res.json(article);
-})
-
-app.get('/hello',(req,res)=>{
-    console.log("Hello");
-    res.send("Hello")
-})
+// first connecting database to node and starting the server
+start();
 
 
-app.listen(8080,()=>{
-    console.log("Server started on port 8080");
-});
+// const articleInfo = [
+//     {name:'learn-react',upvotes : 0, comments:[]},
+//     {name:'learn-node',upvotes : 0, comments:[]},
+//     {name:'learn-mongDb',upvotes : 0, comments:[]},
+// ];
